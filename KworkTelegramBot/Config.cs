ce86e639499cf85acbe8569519220d4e25bc8609
Belachkin <Belachkin@DESKTOP-M2T3353>
2022-10-19 17:20:49 +0300

    add Config class

---
+++ KworkTelegramBot/Config.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KworkTelegramBot
+{
+    internal class Config
+    {
+        public class Category
+        {
+            public const string DesignUrl = "";
+            public const string ItUrl = "";
+            public const string SeoUrl = "";
+            public const string TextUrl = "";
+            public const string AudioVideoUrl = "";
+            public const string BusinessUrl = "";
+        }
+    }
+}
